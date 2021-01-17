6f2f792c0c13842a57ebb700af523174b999177a
nima ladmakhi <cody18v@gmail.com>
2021-01-17 02:22:20 -0800

    BEM-HEADER-DROPDOWN

--- behavior/main.js
+++ behavior/main.js
@@ -1,2 +1,20 @@
 /** @format */
 
+const dropdownHeader = document.querySelector('.header__dropdown');
+
+const dropdownToggler = document.querySelector('[headerDropdownToggle]');
+
+const header = document.querySelector('.header');
+
+header.addEventListener('click', function (event) {
+     const hasAttr = event.target.parentNode.getAttribute('headerDropdownToggle');
+     const isClose = dropdownHeader.classList.contains('d-none');
+     if (hasAttr === null && !isClose) dropdownHeader.classList.add('d-none');
+});
+
+dropdownToggler.addEventListener('click', function () {
+     const isClose = dropdownHeader.classList.contains('d-none');
+     if (isClose) {
+          dropdownHeader.classList.remove('d-none');
+     } else dropdownHeader.classList.add('d-none');
+});
